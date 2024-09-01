30d59f0a68307763481b49e9a4d70446cbef180b
esmrzv <esmurziev.1999@mail.ru>
2024-09-01 21:25:41 +0300

    реализована возможность авторизации полбзователя

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from users import views
+from users.apps import UsersConfig
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.UserCreateAPIView.as_view(), name='user-create'),
+    path('list/', views.UserListAPIView.as_view(), name='user-list'),
+    path('users/<int:pk>/retrieve/', views.UserRetrieveAPIView.as_view(), name='user-retrieve'),
+    path('users/<int:pk>/update/', views.UserUpdateAPIView.as_view(), name='user-update'),
+    path('users/<int:pk>/destroy/', views.UserDestroyAPIView.as_view(), name='user-destroy')
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
